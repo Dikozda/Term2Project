7c3ce8dc8fe8b823de0237789cc9e79ba0146817
Dikozda <Epicgames@ofwa.net>
2018-05-07 17:34:31 +1000

    commented code

--- Source/Term2Project/Private/Grid.h
+++ Source/Term2Project/Private/Grid.h
@@ -6,46 +6,56 @@
 #include "GameFramework/Actor.h"
 #include "Grid.generated.h"
 
+//this is to store the location of each point in the grid, this will be stored in the GridPoints TArray.
 USTRUCT(BlueprintType)
 struct FGridNode
 {
 	GENERATED_BODY()
 
+		//this is the varrible that stores the location of this point.
 		UPROPERTY(BlueprintReadWrite)
 		FVector Location;
 
+	//Set the location varible, had to do this since the constructor complained.
 	void SetLocation(FVector NewLocation)
 	{
 		Location = NewLocation;
 	}
 
+	//the constructor, simply gives the location varible a defualt value 0,0,0.
 	FGridNode()
 	{
 		Location = FVector(0.0f, 0.0f, 0.0f);
 	}
 };
 
+
+//this data struct basicly contains all the requierd infomation needed for the x rows, it has all the grid points along a x line, and the row number.
 USTRUCT(BlueprintType)
 struct FXRow
 {
 	GENERATED_BODY()
 
+		//This is where it stores the gridpoints.
 		UPROPERTY(BlueprintReadOnly)
 		TArray<FGridNode> GridPoints;
 
+	//This stores the current row number, this is basicly what row this is, e.g. this is row 5 and that is row 2.
 	UPROPERTY(BlueprintReadOnly)
 		int RowNumber;
 
+	//ues this to set the RowNumber.
 	void SetRowNumber(int NewRowNumber)
 	{
 		RowNumber = NewRowNumber;
 	}
 
+	//Use this to store the grid points
 	void SetGridPoints(TArray<FGridNode> NewGridPoints)
 	{
 		GridPoints = NewGridPoints;
 	}
-
+	// constructor for construction of the defualt of 1 varible.
 	FXRow()
 	{
 		RowNumber = 0;
